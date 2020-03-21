5c3a4ee0f0c5393ccd7ac8d91f9dd9845e50441f
Fernando <FernandoDefez>
2020-03-21 07:05:48 -0500

    First Commit

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
 window.addEventListener("scroll",  function () {
     const email = this.document.getElementById("email-form");
     const card = this.document.getElementById("card");
-    if(this.scrollY<200 && this.scrollY<600 ){
+    if(this.scrollY<250){
         card.style.animation="rightAnimation 1s ease-in";
         card.style.opacity="1";
     }
-    if(this.scrollY>540 && this.scrollY<1200 ){
-        email.style.animation="leftAnimation 1s ease-in";
-        email.style.opacity="1";
-    }
 });
 
-function openCity(cityName){
-    var i;
-    var tab = document.getElementById("tab-buttons");
-    var x = document.getElementsByClassName("works"); //Devuelve todos los elementos con la clase city en este caso son 3, los oculta todos
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(cityName).style.display = "block"; //Accedo al elemento con la id del nombre del parámetro para mostrarlo
-    tab.style.borderBottom = "none";
-}
+
+var _1st = $('#1stMonthBtn');   var _1stDisplay = $('#1stMonth');
+var _2nd = $('#2ndMonthBtn');   var _2ndDisplay = $('#2ndMonth');
+var _3rd = $('#3rdMonthBtn');   var _3rdDisplay = $('#3rdMonth');
+
+
+$(_1st).click(function(){ 
+    _1stDisplay.toggle();
+    _2ndDisplay.hide();
+    _3rdDisplay.hide();
+});
+
+$(_2nd).click(function(){
+    _1stDisplay.hide();
+    _2ndDisplay.toggle();
+    _3rdDisplay.hide(); 
+});
+
+$(_3rd).click(function(){ 
+    _1stDisplay.hide();
+    _3rdDisplay.toggle();
+    _2ndDisplay.hide();
+});
